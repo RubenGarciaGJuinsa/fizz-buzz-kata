8b29315c42dd0b78caeaf45073d9ed9600bf819e
Rubén García <rubengarcia@grupojuinsa.es>
2020-02-04 17:09:04 +0100

    BLUE: give_nan_throws_exception

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -8,10 +8,19 @@ class FizzBuzz
 {
     public function print($number)
     {
+        $this->checkIfNumberIsAnInteger($number);
+
+        return $number;
+    }
+
+    /**
+     * @param $number
+     * @throws \Exception
+     */
+    protected function checkIfNumberIsAnInteger($number): void
+    {
         if ( ! is_int($number)) {
             throw new \Exception('$number is not an integer');
         }
-
-        return $number;
     }
 }
